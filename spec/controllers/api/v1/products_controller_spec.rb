a9adee9076b79192c0692791f91e25bfb057845a
Mohnish Jadwani <mail4mohnish@gmail.com>
2015-11-14 18:57:56 +0530

    Makes the spec description more meaningful

--- spec/controllers/api/v1/products_controller_spec.rb
+++ spec/controllers/api/v1/products_controller_spec.rb
@@ -3,7 +3,7 @@ require 'rails_helper'
 RSpec.describe Api::V1::ProductsController, type: :controller do
 
   describe '#create' do
-    context "product details that are correctly specified" do
+    context "product details are correctly specified" do
       context "a successful create request" do
         it "increases the number of products by one" do
           product_params = {product: {name: "Macbook air", description: "A laptop", price: 70000}}
