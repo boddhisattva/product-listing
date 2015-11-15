97f2df7b7200fc933a337e467cfc661735d3aec2
Mohnish Jadwani <mail4mohnish@gmail.com>
2015-11-16 04:58:57 +0530

    Adds product model specs

--- spec/models/product_spec.rb
+++ spec/models/product_spec.rb
@@ -1,5 +1,53 @@
 require 'rails_helper'
 
 RSpec.describe Product, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+
+  before do
+    @product = FactoryGirl.create(:product)
+  end
+
+  subject { @product }
+
+  it { should respond_to(:name) }
+  it { should respond_to(:description) }
+  it { should respond_to(:price) }
+
+  it { should be_valid }
+
+  describe "when product name is not present" do
+    context "name is an empty string" do
+      before { @product.name = "" }
+      it { should_not be_valid }
+    end
+
+    context "name is nil" do
+      before { @product.name = nil }
+      it { should_not be_valid }
+    end
+  end
+
+  describe "when product description is not present" do
+    context "description is an empty string" do
+      before { @product.description = "" }
+      it { should_not be_valid }
+    end
+
+    context "description is nil" do
+      before { @product.description = nil }
+      it { should_not be_valid }
+    end
+  end
+
+  describe "when product price is not present" do
+    context "price is an empty string" do
+      before { @product.price = "" }
+      it { should_not be_valid }
+    end
+
+    context "price is nil" do
+      before { @product.price = nil }
+      it { should_not be_valid }
+    end
+  end
+
 end
