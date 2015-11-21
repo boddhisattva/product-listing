77b9073172291f4d5e86db16c270fc3cbc23dd38
Mohnish Jadwani <mail4mohnish@gmail.com>
2015-11-21 20:52:15 +0530

    Adds DB level constraints to ensure product attributes aren't nil

--- db/schema.rb
+++ db/schema.rb
@@ -11,16 +11,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151114004656) do
+ActiveRecord::Schema.define(version: 20151121150222) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
   enable_extension "hstore"
 
   create_table "products", force: true do |t|
-    t.string   "name"
-    t.text     "description"
-    t.float    "price"
+    t.string   "name",        null: false
+    t.text     "description", null: false
+    t.float    "price",       null: false
     t.datetime "created_at"
     t.datetime "updated_at"
   end
